57af7dd3b6baa421f854e631b02c0cea1280dc3e
Alina Laniuk <101401177+AlinaLaniuk@users.noreply.github.com>
2023-11-12 19:12:36 +0200

    Update this-examples.js

--- js/this-examples.js
+++ js/this-examples.js
@@ -25,6 +25,9 @@ setTimeout(theObj.doLogThis, 1);
 // примере выше ссылку на функцию из объекта в какую-то переменную, а потом передать ее в setTimeout
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 function getName(greetings = 'hello', exclamation = '!!!') {
   console.log(`${greetings} ${this.name} ${exclamation}`);
 }
@@ -44,6 +47,9 @@ Function.prototype.customBind = function (newThis, ...boundArgs){//
         return this.call(newThis, ...boundArgs, ...args);   
     }
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // реализация кастомного bind. Обрати внимание на частичное применение (реальный bind тоже так может), а также на то, что возвращаемая функция - стрелка. Можно сделать и через нормальную функцию, однако в этом случае нужно сделать замыкание, сохранить this (функцию, к которой биндим) в переменную в родительском окружении возвращаемой нормальной функции.
 
@@ -66,3 +72,38 @@ boundLogThis = logThis.bind({name: 'kakTebeTakoe!?'});
 new logThis(); // this здесь - пустой объект (из-за new)
 new boundLogThis(); // также будет выведен пустой объект. контекстом boundLogThis будет объект, который был передан в bind. Однако енсли эту функцию вызвать через new, про bind можно забыть. Не надо выдумывать то, чего нет. Мы смотрим на то, как вызывается функция. Фугкция вызывается с ключевым словом new. Этот оператор задает в качестве this пустой объект. 
 new logArrowThis(); // свалимся с ошибкой, нельзя вызвать стрелку с new, так как стрелка не может быть конструктором
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+john = {
+  name: 'john',
+  surname: 'smith',
+
+  getSurname1() { return this.surname; },
+
+  getSurname2: () => this.surname,
+
+  getSurname3: function () { return this.surname },
+
+  getSurname4: function () {
+    const getSurnameInner = () => {
+      return this.surname;
+    }
+
+    return getSurnameInner();
+  },
+
+  getSurname5: function () {
+    function getSurnameInner() {
+      return this.surname;
+    }
+
+    return getSurnameInner();
+  }
+}
+
+// console.log(john.getSurname1()) // 'smith'
+// console.log(john.getSurname2()) // undefined
+// console.log(john.getSurname3()) // 'smith'
+// console.log(john.getSurname4()) // 'smith'
+// console.log(john.getSurname5()) // undefined
