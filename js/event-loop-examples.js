f07320890aaed54d30625294dbf2e27fdea419f0
Alina Laniuk <101401177+AlinaLaniuk@users.noreply.github.com>
2023-11-13 16:02:54 +0200

    Update event-loop-examples.js

--- js/event-loop-examples.js
+++ js/event-loop-examples.js
@@ -26,3 +26,44 @@ Promise.resolve().then(() => {
 log('4');
 
 // 1 - 4 - promise 1 - promise 2 - setTimeout 1 - promise setTimeout - setTimeout 2
+
+
+function log(value){
+    console.log(value);
+};
+
+
+undefined
+log('1');
+
+setTimeout(() => {
+    log('setTimeout 1');
+    Promise.resolve().then(() => {
+        log('promise setTimeout')
+    })
+}, 0);
+
+setTimeout(() => {
+    log('setTimeout 2')
+}, 0);
+
+queueMicrotask(() => {
+    log('queueMicrotask1')
+})
+
+Promise.resolve().then(() => {
+    log('promise 1')
+});
+
+queueMicrotask(() => {
+    log('queueMicrotask2')
+})
+
+
+Promise.resolve().then(() => {
+    log('promise 2')
+});
+
+log('4');
+
+// 1 - 4 - queueMicrotask1 - promise 1 - queueMicrotask2 - promise 2 - setTimeout 1 - promise setTimeout - setTimeout 2
